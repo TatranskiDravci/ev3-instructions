8b9b98703fccbc7de58b8af20238504c07a94099
LukasDrsman <lukaskodr@gmail.com>
2020-07-15 22:40:58 +0200

    Tried to calculate angles. calculations : correct direction : sometimes correct my day : ruined

--- canvas.py
+++ canvas.py
@@ -4,9 +4,12 @@ from time import sleep
 
 tk = tkinter.Tk()
 can = tkinter.Canvas(tk, bg="white", bd=1, width=wi, height=hi, cursor="plus")
-rbt = Robot(can, 40, 40, 40, 40)
+map = tkinter.PhotoImage(file="city_shaper.png")
+can.create_image(0, 0, image=map, anchor=tkinter.NW)
+rbt = Robot(can, 50, 50, 50, 50, 0)
 can.pack()
 can.bind("<Button-1>", rbt.btn_one)
+can.bind("<Button-2>", rbt.btn_two)
 can.bind("<Button-3>", rbt.btn_three)
 
 while(True):
--- objects.py
+++ objects.py
@@ -1,11 +1,12 @@
-hi = 330
-wi = 750
+import math as m
+hi = 668
+wi = 1192
 
 def rect(x, y, w, h, canvas, fill="#000", outline="#000"):
     return canvas.create_rectangle(x, hi-y-h, x+w, hi-y, fill=fill, outline=outline)
 
 class Robot:
-    def __init__(self, canvas, sx, sy, w, h):
+    def __init__(self, canvas, sx, sy, w, h, sang):
         self.path = []
         self.path_lines = []
         self.path.append([sx, sy])
@@ -14,6 +15,7 @@ class Robot:
         self.y = sy
         self.w = w
         self.h = h
+        self.sang = sang
         self.body = rect(self.x - (self.w/2), self.y - (self.h/2), self.w, self.h, self.can, "#eb9771", "#000")
 
     def move(self, x, y):
@@ -24,7 +26,7 @@ class Robot:
         self.path_lines.append(self.can.create_line(
             self.path[len(self.path) - 2][0], (self.path[len(self.path) - 2][1] - hi) * -1,
             x, y,
-            width=2
+            width=6, fill="#622775"
         ))
         print(self.path)
 
@@ -44,5 +46,25 @@ class Robot:
     def btn_one(self, event):
         self.move(event.x, event.y)
 
+    def btn_two(self, event):
+        self.eval()
+
     def btn_three(self, event):
         self.remove()
+
+    def eval(self):
+        prog_str = ""
+        angles = []
+        for i in range(len(self.path)):
+            if(i > 0):
+                if(self.path[i][0] != self.path[i-1][0]):
+                    angle = m.degrees(m.atan((self.path[i][1]- self.path[i-1][1])/(self.path[i][0]- self.path[i-1][0])))
+                elif(self.path[i][1] > self.path[i-1][1]):
+                    angle = 90
+                else:
+                    angle = -90
+                angles.append(angle)
+            else:
+                pass
+
+        print(angles)
