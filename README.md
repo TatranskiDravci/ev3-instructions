7220f1e26f0abb4ed49f0afb7c972432d4476233
LukasDrsman <lukaskodr@gmail.com>
2020-08-03 22:42:38 +0200

    Remade the compiler, added documentation

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # ev3-instructions
+
+A set of tools to simplify building EV3 programs
+
+## Tools
+
+### Constructor
+* Engine: Godot
+* Output: **EPPF** (ev3 program path file) and **DAT** file
+
+### Compiler
+* Language: python3
+* Input: **EPPF** files
+* Output: **PY** (python) files based on data specified by eppfc.json
+
+## Specifications
+### EPPF (ev3 program path file)
+* defines actions of EV3 unit
+* configurable
+* functions
+  * no multiple equal functions on single line
+  * name cannot include any special, numeric or lower designated characters
+  * parameters enclosed in function's name: `r123r` or `mv23::2mv`
+  * function parameters separated by `::`
+  * list parameters enclosed in `[]` and separated by `,`
+
+### .eppfc.json
+* dotfile
+* sourced from `~` directory
+* written in JSON
+* `Config` defines program configuration
+* `Class` defines robot class
+  * `Methods` defines all methods and their parameters
+* `_n` defines parameter n passed on by matching **EPPF** function
--- constructor/robot.gd
+++ constructor/robot.gd
@@ -6,6 +6,23 @@ var mat
 var cPos
 var angLock
 
+func listf(path):
+	var files = []
+	var dir = Directory.new()
+	dir.open(path)
+	dir.list_dir_begin()
+
+	while true:
+		var file = dir.get_next()
+		if file == "":
+			break
+		elif not file.begins_with("."):
+			files.append(file)
+
+	dir.list_dir_end()
+
+	return files
+
 func px2cm(px):
 	return (px * 202.0/1196.0)
 
@@ -53,19 +70,19 @@ func _input(event):
 			if event.scancode == KEY_S and event.pressed:
 				print("saving")
 				var fPol = File.new()
-				var fCart = File.new()
-				var id = "/home/lukas/.ev3-paths/" + str((randi()%200+1)) + str((randi()%200+1)) + str((randi()%200+1)) + str((randi()%200+1))
-				var idPol = id + "polar.rppf"
-				var idCart = id + "cartesian.rcpf"
+				var fDat = File.new()
+				var id = "user://path" + str(len(listf("user://"))/2)
+				var idPol = id + ".eppf"
+				var idDat = id + ".dat"
 				var polar = ""
-				var cart = str(mat.x) + " " + str(mat.y) + "\n"
+				var dat = str(mat.x) + " " + str(mat.y) + "\n"
 				var i = 0
 				for loc in locs:
 					if i > 0:
 						polar = polar + "r" + str(int(rad2deg(loc[2]) - rad2deg(locs[i-1][2]))) + "r m" + str(px2cm(loc[3])) + "m\n"
 					else:
 						polar = polar + "r" + str(int(rad2deg(loc[2]))) + "r m" + str(px2cm(loc[3])) + "m\n"
-					cart = cart + str(loc[0]) + " " + str(loc[1]) + "\n"
+					dat = dat + str(loc[0]) + " " + str(loc[1]) + str(loc[2]) + str(loc[3]) + "\n"
 					i = i + 1
 
 				# save "polar" coordinates
@@ -74,8 +91,8 @@ func _input(event):
 				fPol.store_string(polar)
 				fPol.close()
 
-				# save cartesian coordinates
+				# save coordinate data
 
-				fCart.open(idCart, File.WRITE)
-				fCart.store_string(cart)
-				fCart.close()
+				fDat.open(idDat, File.WRITE)
+				fDat.store_string(dat)
+				fDat.close()
